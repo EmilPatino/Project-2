6dcc0da218c948a5a1dbbdcd9ca8f8152ff872d5
aaron <aaronbushman@gmail.com>
2019-08-24 12:41:57 -0400

    schema for mysql server

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS cmb_db;
+DROP DATABASE IF EXISTS crm_db;
 -- Creates the "blogger" database --
-CREATE DATABASE cmb_db;
-USE cmb_db;
+CREATE DATABASE crm_db;
+USE crm_db;
