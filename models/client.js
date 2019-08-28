ab5a4181f2f66903c8a2b513ee224afb10a026b6
aaron <aaronbushman@gmail.com>
2019-08-27 21:13:33 -0400

    add model for mysql table data

---
+++ models/client.js
@@ -0,0 +1,37 @@
+module.exports = function (sequelize, DataTypes) {
+  var Client = sequelize.define("clients", {
+    clientName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    clientAddress: {
+      type: DataTypes.STRING
+    },
+    clientState: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      },
+    },
+    clientEmail: {
+      type: DataTypes.STRING
+    },
+    clientPhone: {
+      type: DataTypes.STRING
+    },
+    contactName: {
+      type: DataTypes.STRING
+    },
+    clientPriority: {
+      type: DataTypes.STRING
+    },
+    clientValue: {
+      type: DataTypes.STRING
+    }
+  });
+  return Client;
+};
